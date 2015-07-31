858ec1e9b30cf64d2e62f717dec6e0dc748c39ff
firsachi <firsachi.alex@gmail.com>
2015-07-31 16:04:45 +0300

    new result

--- src/job4/Result.java
+++ src/job4/Result.java
@@ -1,11 +1,12 @@
 package job4;
 
+
 import java.util.ArrayList;
 
 public class Result {
 	
 	//private Object[] obj;
-	private ArrayList<Object> mass=new ArrayList<Object>();
+	private ArrayList<Things> mass=new ArrayList<Things>();
 	
 	public Result(String path) {
 		String[] array=InputFile.read(path);
@@ -14,38 +15,14 @@ public class Result {
 		}else{
 			for(int i=0;i<array.length;i++){
 				if(getEven(getDouble(array[i]))){
-					mass.add("Обложка желтого цвета ");
-					mass.add("Цена ");
-					mass.add(getDouble(array[i])/2 + " ");
-				}else{
-					addNullArrays(3);
+					mass.add(Books.getBook(array[i]));	
 				}
 				if(getThreeShare(getDouble(array[i]))){
-					mass.add("Свитер ");
-					mass.add("цвет  черный ");
-					mass.add("цена "+ getDouble(array[i])/2.5 +" ");
-				}else{
-					addNullArrays(3);
+					mass.add(Clothes.getClothes(array[i]));
 				}
 				if(getThreeShare(getDouble(array[i])) && getEven(getDouble(array[i]))){
-					mass.add("Кроссовки");
-					mass.add("цвет крвсный ");
-					switch (i){
-						case 0:
-							mass.add("цена "+getDouble(array[0])/2);
-						break;
-						case 9:
-							mass.add("цена "+getDouble(array[9])/2);
-						break;
-						default:
-							mass.add("цена "+(getDouble(array[i-1])+getDouble(array[i+1]))/2);
-						break;
-					}
-				}else{
-					addNullArrays(3);
+					mass.add(Shoes.getShoes(array[i]));
 				}
-				mass.add(" ");
-				System.out.println(i);
 			}
 			OutputFile.write(mass, "OutputData.txt");
 		}
@@ -72,9 +49,4 @@ public class Result {
 		return resultDouble;
 	}
 	
-	private void addNullArrays(int number){
-		for(int i=0;i<number;i++){
-			mass.add("_ ");
-		}
-	}
 }
